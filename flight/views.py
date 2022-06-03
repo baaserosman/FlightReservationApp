29096fe9b8323320756725374cd9cbba91456cee
baaserosman <baaser.osman@gmail.com>
2022-06-03 10:51:46 +0200

    permissions, serializers, urls, views updated

--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Flight, Reservation
+from .serializers import FlightSerializer, ReservationSerializer
+from rest_framework import viewsets
+from .permissions import IsStufforReadOnly
 
-# Create your views here.
+
+class FlightView(viewsets.ModelViewSet):  # GET, POST, UPDATE, DELETE, PATCH
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    permission_classes = (IsStufforReadOnly,)
+    
+    
+class ReservationView(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
